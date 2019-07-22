95bca12d1b1fdc5308371b0d1367339d4b9a1540
brainliu <406096277@qq.com>
2019-07-22 23:16:40 +0800

    前向传播法计算完毕

--- LSTM-源码学习.py
+++ LSTM-源码学习.py
@@ -34,9 +34,22 @@ class LstmLayer(object):
         self.h_list=self.init_state_vec()
         #各时刻的遗忘门f
         self.f_list=self.init_state_vec()
-        #各时刻的输入们i
+        #各时刻的输入门
         self.i_list=self.init_state_vec()
-        
+        #各时刻的输出门
+        self.o_list=self.init_state_vec()
+        #各时刻的即时状态
+        self.ct_list=self.init_state_vec()
+
+        #遗忘门权重矩阵wfh，wfx，偏置项bf
+        self.wfh,self.wfx,self.bf=(self.init_weight_mat())
+        #输入门权重矩阵wif，wix，偏置项bi
+        self.wih,self.wix,self.bi=(self.init_weight_mat())
+        #输出门权重矩阵wof，wox，偏置项bo
+        self.woh,self.wox,self.bo=(self.init_weight_mat())
+
+        #单元状态权重矩阵wch,wcx偏置项bc
+        self.wch,self.wcx,self.wbc=(self.init_weight_mat())
 
     def init_state_vec(self):
         '''
@@ -57,3 +70,41 @@ class LstmLayer(object):
                                (self.state_with, self.input_with))
         b = np.zeros((self.state_with, 1))
         return Wh, Wx, b
+
+    #forward 方法实lstm的前向计算
+    def forward(self,x):
+        """
+        根据1-式-6式进行前向计算
+        :param self:
+        :param x:
+        :return:
+        """
+        self.times+=1
+        #遗忘门
+        fg=self.calc_gate(x,self.wfx,self.wfh,self.bf,self.gate_activator)
+        #输入门
+        ig=self.calc_gate(x,self.wix,self.wih,self.bi,self.gate_activator)
+        #输出门
+        og=self.calc_gate(x,self.wox,self.woh,self.bo,self.gate_activator)
+
+        #各时刻的输出门添加到list中去记录下来
+        self.o_list.append(og)
+
+        #即时状态
+        ct=self.calc_gate(x,self.wcx,self.wch,self.bc,self.output_activator)
+        self.ct_list.append(ct)
+
+        #单元状态
+        c=fg*self.ct_list[self.times-1]+ig*ct
+        self.c_list.append(c)
+        #输出
+        h=og*self.output_activator.forward(c)
+        self.h_list.append(h)
+
+          
+    def calc_gate(self,x,wx,wh,b,activator):
+        """计算门"""
+        h=self.h_list[self.times-1]#上次lstm的输出
+        net=np.dot(wh,h)+np.dot(wx,x)+b
+        gate=activator.forward(net)
+        return gate
