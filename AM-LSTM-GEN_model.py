b01298850b7899d013593205c72f6d3a9e633074
brainliu <406096277@qq.com>
2019-08-09 17:15:36 +0800

    1.完成了公式的梳理，下一步进行引言和文献综述的撰写！

---
+++ AM-LSTM-GEN_model.py
@@ -0,0 +1,8 @@
+#-*-coding:utf8-*-
+#user:brian
+#created_at:2019/8/9 17:13
+# file: AM-LSTM-GEN_model.py
+#location: china chengdu 610000
+####公式部分已经梳理完毕，下周之内完成文献综述和引言部分8.12-19
+####下下周8.20-27完成实验部分
+###预计八月底完成初稿，带着论文去开学啦！！！
