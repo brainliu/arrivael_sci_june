f65c885a932fab4c8110cecd3abbedd43eb0280a
brainliu <406096277@qq.com>
2019-08-27 20:54:18 +0800

    开始处理数据，构建算法模型 1，关于未来的旅客人数，是预测实时的还是预测一天的？ 2，预测一天的，然后建立时刻表，航班时刻是可以变化的 3，吸引力机制 4，记忆单元 5.joint trainning 6.开始写第二个和第三个公式了！0806 7.0825环境配置ok，然后开始写程序提取数据做算法了 8.0827 begin 先回忆思路 9.

--- test_study/keras快速入门.py
+++ test_study/keras快速入门.py
@@ -28,3 +28,22 @@ model = tf.keras.Model(inputs=inputs, outputs=outputs, name='mnist model')
 
 model.summary()
 
+x = tf.ones((3, 5))
+inputdata={}
+x = [[3.]]
+m = tf.matmul(x, x)
+print(m.numpy())
+
+a = tf.constant([[1,9],[3,6]])
+print(a)
+
+import tensorflow as tf
+my_var = tf.Variable(tf.ones([2,3]))
+print(my_var)
+try:
+    with tf.device("/device:GPU:0"):
+        v = tf.Variable(tf.zeros([10, 10]))
+        print(v)
+except:
+    print('no gpu')
+
