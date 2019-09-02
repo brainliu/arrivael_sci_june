fbfa707ccb438a128821424234fc62825dc70e06
brainliu <406096277@qq.com>
2019-09-02 17:03:11 +0800

    1.增加了generator 2.构建了一个第一版本的简单的模型 3.看看周末能不能把数据灌进去试试看呢，下一步把generator的代买落地实现了，后面就可以解析了！ 4.第一步关键是用测试数据，让现在的模型跑起来哦！！！ 5,2019-09-01处理好生成器，以及生成器的训练参数

--- data_deal.py
+++ data_deal.py
@@ -8,12 +8,16 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 ########################！！！！先不考虑变化参数的问题###############################
 
 ######1.历史数据解析以及对过去的数据进行统计################################
-######1.统计得到每一天每分钟的航班人数
+
+
+
+######1.统计得到每一天每分钟的航班人数，历史数据
 def get_minute_one_count():
-    ##实现一个参数，也就是5分钟、10分钟、15分钟、30分钟 多种组合规则的提取程序
+    ##得到某一天的1440的每分钟的人数
     #3输出字段：一个是时间、一个是人数、一个是日期
     pass
 
@@ -23,11 +27,47 @@ def get_inputs_past_w_day_data():
     ##配置：单个序列的长度、以及每个吸引力的长度
     pass
 
+def get_aggravte_minute():
+    ##得到每分钟的聚合人数
+    pass
 
+
+def get_hb_schedule():
+    ##处理数据得到所有的航班计划
+    pass
+
+
+
+data=pd.read_csv("./data_ori/flight_filted6.csv")
+hb_schedule=data.drop_duplicates(subset=["SCHETIME"])
+hb_schedule_all=hb_schedule[["SCHETIME"]]
+hb_schedule_all.to_csv("./data_ori/hb_all_table.csv")
+##6月份有407个航班 里面可能有重复的时间 200个时间有航班，只计算航班时间对应人数就行了，简化计算一下
+#对每一天生成一个list 长度为407的 代表这407个航班是否存在的序列
+
+hb_time_all=sorted(list(hb_schedule_all["SCHETIME"]))
+##将所有数据汇总得到407个航班时间和航班人数，对应不同的航空公司
+##这里面需要一个判断逻辑
+def get_every_day_inout_schedule():
+    #对每一天的数据进行处理，得到6月份每一天的航班计划输入格式
+    #200*1
+    pass
+
+
+hb_data_one_Day=data[data["SCHETIME_date"]==13]
+sums=hb_data_one_Day["CHECKINTIME"].groupby([hb_data_one_Day["SCHETIME"]]).count()
+index_time=hb_data_one_Day.drop_duplicates("SCHETIME")["SCHETIME"].sort_values()
+index_people=list(sums)
+resut_hb_people=[0 for i in hb_time_all]
+
+
+print(sums)
 ##############2.航班规律到达函数##########################
 #单航班到达规律生成器
 def possion_generator(hb_time,hb_people):
-
+    ###可能存在需要训练的参数
+    ###1） 时间比例和人数比例 一共165分钟，w1+w2+w3=1  w2变化  w1+w3=1-w2    *165
+    ###2) 人数比例                         w1+w2+w3=1  w2变化   w1=w3=（1-w2）/2  *hb_people
     time_people=[0 for i in range(1440)]
     ##分成三段叠加
     #时间上分段45-210分钟，1==》45-70  2===》 70-150  3===》150-210  三段之间的比例计算为
@@ -35,61 +75,69 @@ def possion_generator(hb_time,hb_people):
     ##先生成到达时间，从第一段低密度开始，最远的地方，距离航班起飞最远到最近45分钟，截至人数全部到达
     # 初始化三个时间区间长度
     ##整体的时间长度，后面通过泊松分布算出来的时间分布可能高于这个值呢
-    ##从第45分钟开始
-    time_interval_low1=25
-    time_interval_high2=80
-    time_interval_low3=60
-    ###上面三个为时间的跨度
-    time_scale_last=[0,0,0]   #记录最后一个值为多少
-    times_scale_chen=[0,0,0]
-    time_arrival_all=[]       #记录每个人的到达时间间隔时间，一共有多少个人人就有多少个数据，最后再把这些数据映射到1440上面去！
-    #高密度和低密度的比例
-    low_percent=0.25
-    high_percent=0.5
-    low_people_1=int(low_percent*hb_people)
-    high_peole_2 = int(high_percent * hb_people)
-    low_people_3=low_people_1
+    if hb_people>0:
+        ##从第45分钟开始
+        time_interval_low1=25
+        time_interval_high2=80
+        time_interval_low3=60
+        ###上面三个为时间的跨度
+        time_scale_last=[0,0,0]   #记录最后一个值为多少
+        times_scale_chen=[0,0,0]
+        time_arrival_all=[]       #记录每个人的到达时间间隔时间，一共有多少个人人就有多少个数据，最后再把这些数据映射到1440上面去！
+        #高密度和低密度的比例
+        low_percent=0.25
+        high_percent=0.5
+        low_people_1=int(low_percent*hb_people)
+        high_peole_2 = int(high_percent * hb_people)
+        low_people_3=low_people_1
 
-    ##计算每个人的到达时间  距离起飞还有45分钟-80分钟的人数的到达时间
-    temp_arrival_time1=hb_time-210+45 #每一段的开始时间 （第一段）
-    temp_arrival_time2=temp_arrival_time1+time_interval_low1  #（第二段 高密度，开始时间）
-    temp_arrival_time3=temp_arrival_time2+time_interval_high2  #（第三段， 低密度，开始时间)
-    result=[]
+        ##计算每个人的到达时间  距离起飞还有45分钟-80分钟的人数的到达时间
+        temp_arrival_time1=hb_time-210+45 #每一段的开始时间 （第一段）
+        temp_arrival_time2=temp_arrival_time1+time_interval_low1  #（第二段 高密度，开始时间）
+        temp_arrival_time3=temp_arrival_time2+time_interval_high2  #（第三段， 低密度，开始时间)
+        result=[]
 
-    for i in range(low_people_1):
-        next_times_Arrival=np.random.exponential(1)
-        time_arrival_all.append(next_times_Arrival)
-        time_scale_last[0]+=next_times_Arrival
-    for i in range(high_peole_2):
-        next_times_Arrival = np.random.exponential(1)
-        time_arrival_all.append(next_times_Arrival)
-        time_scale_last[1] += next_times_Arrival
-    for i in range(low_people_3):
-        next_times_Arrival = np.random.exponential(1)
-        time_arrival_all.append(next_times_Arrival)
-        time_scale_last[2] += next_times_Arrival
-    ##将得到的规模进行转换
-    times_scale_chen[0]=float(time_interval_low1/time_scale_last[0])
-    times_scale_chen[1] = float(time_interval_high2 / time_scale_last[1])
-    times_scale_chen[2] = float(time_interval_low3 / time_scale_last[2])
-    for j in range(3):
-        print(times_scale_chen[j])
+        for i in range(low_people_1):
+            next_times_Arrival=np.random.exponential(1)
+            time_arrival_all.append(next_times_Arrival)
+            time_scale_last[0]+=next_times_Arrival
+        for i in range(high_peole_2):
+            next_times_Arrival = np.random.exponential(1)
+            time_arrival_all.append(next_times_Arrival)
+            time_scale_last[1] += next_times_Arrival
+        for i in range(low_people_3):
+            next_times_Arrival = np.random.exponential(1)
+            time_arrival_all.append(next_times_Arrival)
+            time_scale_last[2] += next_times_Arrival
+        ##将得到的规模进行转换
+        times_scale_chen[0]=float(time_interval_low1/time_scale_last[0])
+        times_scale_chen[1] = float(time_interval_high2 / time_scale_last[1])
+        times_scale_chen[2] = float(time_interval_low3 / time_scale_last[2])
+        for j in range(3):
+            print(times_scale_chen[j])
 
-    for i in range(low_people_1):
-        temp_arrival_time1+=time_arrival_all[i]*times_scale_chen[0]
-        result.append(temp_arrival_time1)
-    for i in range(low_people_1,high_peole_2+low_people_1):
-        temp_arrival_time2+=time_arrival_all[i]*times_scale_chen[1]
-        result.append(temp_arrival_time2)
-    for i in range(high_peole_2+low_people_1,hb_people):
-        temp_arrival_time3+=time_arrival_all[i]*times_scale_chen[2]
-        result.append(temp_arrival_time3)
-    ##现在的result就是每个人的到达时间，下一步就是进行排序和统计每分钟的人数：
-    for k in result:
-        time_people[int(k)]+=1
-    return time_people
-###多航班叠加
+        for i in range(low_people_1):
+            temp_arrival_time1+=time_arrival_all[i]*times_scale_chen[0]
+            result.append(temp_arrival_time1)
+        for i in range(low_people_1,high_peole_2+low_people_1):
+            temp_arrival_time2+=time_arrival_all[i]*times_scale_chen[1]
+            result.append(temp_arrival_time2)
+        for i in range(high_peole_2+low_people_1,hb_people):
+            temp_arrival_time3+=time_arrival_all[i]*times_scale_chen[2]
+            result.append(temp_arrival_time3)
+        ##现在的result就是每个人的到达时间，下一步就是进行排序和统计每分钟的人数：
+        for k in result:
+            time_people[int(k)]+=1
+        return time_people
+    else:
+        return time_people
+###多航班叠加 统计每一天的所有航班的总人数
 def multi_flight(hb_time_list,hb_people_list):
+    """
+    :param hb_time_list: 某一天的所有航班的航班时间
+    :param hb_people_list: 某一天的所有的航班的人数
+    :return:某一天的航班计划得到的航班人数  1440*1 维度为list
+    """
     result2=[0 for i in range(1440)]
     for i in range(len(hb_time_list)):
         temp=possion_generator(hb_time_list[i],hb_people_list[i])
@@ -103,7 +151,7 @@ def plot(data,name):
     y = range(1440)
     plt.plot(y, data, ls='dashed',
              lw=2, c='r', label='Poisson distribution\n$(\lambda=%s)$'%name)
-    #plt.xlim(200,400)
+    plt.xlim(200,400)
     plt.savefig("test.png")
     plt.show()
 
---
+++ test_study/测试代码编辑.py
@@ -0,0 +1,7 @@
+#-*-coding:utf8-*-
+#user:brian
+#created_at:2019/9/2 16:13
+# file: 测试代码编辑.py
+#location: china chengdu 610000
+for i in range(0):
+    print(i)
